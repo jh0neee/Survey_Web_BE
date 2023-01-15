af2c6bf8632b2983a0d1ab9a45b1b45f50e61082
지현 <jh0neee@naver.com>
2023-01-15 21:50:13 +0900

    db connect

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
-const express = require('express');
-const port = 5000;
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const { PORT, MONGO_URL } = process.env;
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const postsRouter = require("./routes/posts-routes");
 const usersRouter = require("./routes/users-routes");
-const HttpError = require('./models/http-error');
+const HttpError = require("./models/http-error");
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => app.listen(PORT))
+  .catch((err) => console.log(err));
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB Connected");
+});
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/posts', postsRouter);
-app.use('/api/user', usersRouter);
+app.use("/api/posts", postsRouter);
+app.use("/api/user", usersRouter);
 
 // 지원되지 않은 routes에 대한 오류처리
 app.use((req, res, next) => {
-  const error = new HttpError('route를 찾지 못했습니다.');
+  const error = new HttpError("route를 찾지 못했습니다.");
   throw error;
-})
-
-app.use((error, req, res, next) => {
-  if(res.headerSent) {
-    return next(error);
-  }
-  res.status(error.code || 500).json({message: error.message || '알 수 없는 오류입니다.'});
-
 });
 
-app.listen(port);
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "알 수 없는 오류입니다." });
+});
