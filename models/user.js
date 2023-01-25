b9e3de706d0d546a208f487b126b8b75058669ff
지현 <jh0neee@naver.com>
2023-01-25 14:59:16 +0900

    [fix] modify user model to delete from database

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 8 },
+  posts: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Post' }]
 });
 
 userSchema.plugin(uniqueValidator);
