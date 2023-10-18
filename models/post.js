11ef46cda0e54872ac2dc2e097939750e84b0e6c
지현 <jh0neee@naver.com>
2023-10-18 17:14:53 +0900

    update createdDate in post model

--- models/post.js
+++ models/post.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
   title: { type: String, required: true },
   author: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
-  createDate: { type: String, default: new Date() },
+  createDate: { type: Date, default: Date.now },
   content: { type: String, required: true },
   surveys: [{ type: mongoose.Types.ObjectId, required: true, ref: "Survey" }],
 });
